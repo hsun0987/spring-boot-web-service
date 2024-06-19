f6235e8750b794b0f3e0f72610a8719ee574b99a
한민선 <hsun0987@daum.net>
2024-06-19 21:02:16 +0900

    4.3 게시글 등록 화면 만들기 - header, footer 나누기 : 페이지 로딩속도를 높이기 위해 css = header, js = footer
    1) index.mustache 화면에서 글 등록 버튼을 누를 시 게시글 등록 화면으로 이동
    - 게시글 등록 화면 생성(posts-save.mustache)
    
    2) posts-save.mustache 화면에서 취소 버튼을 누르면 /(index) 로 이동
    
    3) posts-save.mustache 화면에서 등록 버튼을 누르면 게시글이 등록
    * 등록 버튼 기능 생성
    - static/js/app/index.js 생성
    ? var main = {} 이렇게 쓰는 이유
    : var index 객체를 만들어 객체 안에서만 function이 유효하기 때문에 다른 JS와 겹칠 위험 방지
    
    4) 등록 후 DB  데이터 확인
    - h2-console 이용

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package org.example.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
     @GetMapping("/")
@@ -10,5 +12,10 @@ public class IndexController {
         // 머스테치 스타터로 인해서 앞경로와 뒤 파일 확장자가 자동 지정
         // src/main/resources/templates/ + "index" + .mustache 전달 -> view resolver 처리
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
