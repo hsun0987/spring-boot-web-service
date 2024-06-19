8d9048d1f92907e5b5e7237ff8fd9ccdf95c8503
한민선 <hsun0987@daum.net>
2024-06-19 20:27:40 +0900

    4.2 기본 페이지 만들기
    - 머스테치 플러그인 설치
    - build.gradle에 의존성 추가
    - src/main/resources/templates/index.mustache 생성
    - index에 간단한 html 코드 추가
    - index controller 생성 후 url 매핑
    - 브라우저 확인

---
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package org.example.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        // 머스테치 스타터로 인해서 앞경로와 뒤 파일 확장자가 자동 지정
+        // src/main/resources/templates/ + "index" + .mustache 전달 -> view resolver 처리
+        return "index";
+    }
+}
