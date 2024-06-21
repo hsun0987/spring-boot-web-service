c6fb24cad25a5be83ce33a6d498fd04ffd0341d0
한민선 <hsun0987@daum.net>
2024-06-21 15:09:50 +0900

    5.3 구글 로그인 연동하기 (3) 로그인 테스트
    #1. index.mustache UI 변경
    - 파일에 로그인 버튼과 성공시 사용자 이름 표시
    #2. index controller 수정
    - 세션을 이용하여 user name을 model에 저장하여 전달
    #3. 테스트
    - 로그인 하기
    - 권한확인 > 게스트 일 때 글작성 x
    - DB 에서 권한 User 로 바꾼 후 글작성 확인

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체 저장
         // postsService.findAllDesc() -> posts 로 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        
         // 머스테치 스타터로 인해서 앞경로와 뒤 파일 확장자가 자동 지정
         // src/main/resources/templates/ + "index" + .mustache 전달 -> view resolver 처리
         return "index";
