260420f2b38ffee6dd517afea7367a3fc8dd5505
한민선 <hsun0987@daum.net>
2024-06-24 16:21:34 +0900

    5.6 네이버 로그인
    #1. 네이버 open API 등록 (application-oauth.properties)
    - 네이버에서는 스프링 시큐리티를 공식 지원하지 않기 때문에 수동으로 입력해줘야함
    #2. 스프링 시큐리티 설정 등록 (OAuthAttributes)
    - 네이버인지 판단하는 코드와 네이버 생성자 추가
    - respose를 user_name으로 지정하여 가져옴
    #3. index.mustache 에 네이버 로그인 버튼 추가

--- src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,15 +24,31 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보 Map을 하나씩 반환
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    private static  OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
